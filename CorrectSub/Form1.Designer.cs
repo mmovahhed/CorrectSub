5df11e7858ff41bf28636684588037418b62dd9a
Red Ribbon <Red Ribbon@PANIC-ROOM>
2022-04-03 11:43:15 +0430

    Add project files.

---
+++ CorrectSub/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace CorrectSub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(318, 51);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.textBox1_DragOver);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(336, 10);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(318, 65);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(336, 40);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(76, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 141);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ CorrectSub/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CorrectSub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "";
+        }
+
+        private void textBox1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        public string[] files;
+        public static string[] stringSeparators = new string[] { "\\" };
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+            files = null;
+            files = e.Data.GetData(DataFormats.FileDrop) as string[]; // get all files droppeds  
+
+            if (files != null && files.Any())
+            {
+                foreach (var item in files)
+                {
+                    string fileName = item.Split(stringSeparators, StringSplitOptions.None).Last();
+                    textBox1.Text += "[" + fileName + "] ";
+                    richTextBox1.Text += Environment.NewLine + fileName + " -->> imported!";
+                }
+                //textBox1.Text = files.First(); //select the first one
+                //   richTextBox1.Text= files
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    textBox1.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                foreach (var item in files)
+                {
+                    string fileName = item.Split(stringSeparators, StringSplitOptions.None).Last();
+                    if (fileName.EndsWith(".txt") || fileName.EndsWith(".srt"))
+                    {
+                        string text = File.ReadAllText(item);
+                        text = text.Replace("‫", " ");
+                        File.WriteAllText(item, text);
+                        richTextBox1.Text += Environment.NewLine + fileName + " -->> replacement sueccessed!";
+                    }
+                }
+
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //if (!String.IsNullOrEmpty(textBox1.Text))
+            //{
+            //    string ext = Path.GetExtension(textBox1.Text);
+            //    richTextBox1.Text = "file type: " + ext;
+
+            //}
+            //else
+            //{
+            //    richTextBox1.Text = "";
+            //}
+        }
+    }
+}
